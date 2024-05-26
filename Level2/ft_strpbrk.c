b33232833bded72e9b2e0922d3255c70b6855acc
PC Efrain <eevisconti09@hotmail.com>
2024-05-27 01:18:48 +0200

    correction ft_strpbrk

--- Level2/ft_strpbrk.c
+++ Level2/ft_strpbrk.c
@@ -14,17 +14,20 @@
 
 char	*ft_strpbrk(const char *s1, const char *s2)
 {
-	if (!s1 || !s2)
-		return (0);
-	while (s2)
+	int	i;
+	int	j;
+
+	i = 0;
+	while (s1[i] != 0)
 	{
-		while (s1)
+		j = 0;
+		while (s2[j] != 0)	
 		{
-			if (*s1 == *s2)
-				return ((char *)s1);
-			s1++;
+			if (s1[i] == s2[j])
+				return ((char *)&s1[i]);
+			j++;
 		}
-		s2++;
+		i++;
 	}
 	return (0);
 }
